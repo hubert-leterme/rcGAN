0b84720baaa097eabef89e937daa18802d890569
Hubert Leterme (from Feynman) <hubert.leterme@cea.fr>
2026-02-09 11:52:00 +0100

    Bugfix in slurm bash script (absolute path for the standard output)

--- mass_map_utils/jobs/training/training_cGAN_mass_map.sh
+++ mass_map_utils/jobs/training/training_cGAN_mass_map.sh
@@ -7,10 +7,11 @@
 #SBATCH --gres=gpu:4g.20gb
 #SBATCH --time=50:00:00
 #SBATCH --mem=50GB
-#SBATCH --output ~/work/Log/slurm_out/out_%j.log
+#SBATCH --output /feynman/home/dap/lcs/hl285110/work/Log/slurm_out/out_%j.log
 
 # Load conda and activate environment
 conda activate mmgan_python310
+wandb login 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
 
 cd /feynman/home/dap/lcs/hl285110/Documents/Code/rcGAN
 
